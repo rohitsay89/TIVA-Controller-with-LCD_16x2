b2e44b7b4fb504c7a259fda7f7f2456498d4b1a1
Rohit Deshpande <rohitsay89@gmail.com>
2015-10-12 19:21:45 -0700

    Comments updated
    updated and indented most of the comments so as to get a clearer understanding of the code.

--- lcd_16x2_code.c
+++ lcd_16x2_code.c
@@ -27,47 +27,42 @@
 addr  00 01 02 03 04 05 ... 0F
 */
 
-#define E  0x20 // on PE5
-#define RS 0x10 // on PE4
-#define LCDDATA (*((volatile uint32_t *)0x400613FC))   // PORTk
-#define LCDCMD (*((volatile uint32_t *)0x4005C0C0))    // PE5,4
+#define E  0x20 							/* on PE5 */
+#define RS 0x10 							/* on PE4 */
+#define LCDDATA (*((volatile uint32_t *)0x400613FC))			/* PORTk */
+#define LCDCMD (*((volatile uint32_t *)0x4005C0C0))			/* PE5,4 */
 
 void OutCmd(unsigned char command){
   LCDDATA = command;
-  LCDCMD = 0;           // E=0, R/W=0, RS=0
-  SysCtlDelay(240);			//SysTick_Wait(T6us);   // wait 6us
-  LCDCMD = E;           // E=1, R/W=0, RS=0
-  SysCtlDelay(240);			//SysTick_Wait(T6us);   // wait 6us
-  LCDCMD = 0;           // E=0, R/W=0, RS=0
-  SysCtlDelay(1600);			//SysTick_Wait(T40us);  // wait 40us
+  LCDCMD = 0;           						/* E=0, R/W=0, RS=0 */
+  SysCtlDelay(240);							/* SysTick_Wait(T6us); wait 6us */
+  LCDCMD = E;								/* E=1, R/W=0, RS=0 */
+  SysCtlDelay(240);							/* SysTick_Wait(T6us); wait 6us */
+  LCDCMD = 0;								/* E=0, R/W=0, RS=0 */
+  SysCtlDelay(1600);							/* SysTick_Wait(T40us); wait 40us */
 }
-// Output a character to the LCD
-// Inputs: letter is ASCII character, 0 to 0x7F
-// Outputs: none
+
+/* Output a character to the LCD, Inputs: letter is ASCII character, 0 to 0x7F, Outputs: none */
 void LCD_OutChar(unsigned char letter){
   LCDDATA = letter;
-  LCDCMD = RS;          // E=0, R/W=0, RS=1
-  SysCtlDelay(240);			//SysTick_Wait(T6us);   // wait 6us
-  LCDCMD = E+RS;        // E=1, R/W=0, RS=1
-  SysCtlDelay(240);			//SysTick_Wait(T6us);   // wait 6us
-  LCDCMD = RS;          // E=0, R/W=0, RS=1
-  SysCtlDelay(1600);			//SysTick_Wait(T40us);  // wait 40us
+  LCDCMD = RS;								/* E=0, R/W=0, RS=1 */
+  SysCtlDelay(240);							/* SysTick_Wait(T6us); wait 6us */
+  LCDCMD = E+RS;							/* E=1, R/W=0, RS=1 */
+  SysCtlDelay(240);							/* SysTick_Wait(T6us); wait 6us */
+  LCDCMD = RS;								/* E=0, R/W=0, RS=1 */ 
+  SysCtlDelay(1600);							/* SysTick_Wait(T40us); wait 40us */
 }
 
-// Clear the LCD
-// Inputs: none
-// Outputs: none
+/* Clear the LCD, Inputs: none, Outputs: none */
 void LCD_Clear(void){
-  OutCmd(0x01);          // Clear Display
-  SysCtlDelay(64000);			//SysTick_Wait(T1600us); // wait 1.6ms
-  OutCmd(0x02);          // Cursor to home
-  SysCtlDelay(64000);		 //SysTick_Wait(T1600us); // wait 1.6ms
+  OutCmd(0x01);								/* Clear Display */
+  SysCtlDelay(64000);							/* SysTick_Wait(T1600us); wait 1.6ms */
+  OutCmd(0x02);								/* Cursor to home */
+  SysCtlDelay(64000);							/* SysTick_Wait(T1600us); wait 1.6ms */
 }
 
-//------------LCD_OutString------------
-// Output String (NULL termination)
-// Input: pointer to a NULL-terminated string to be transferred
-// Output: none
+/* ============================ LCD_OutString ============================  */
+/* Output String (NULL termination), Input: pointer to a NULL-terminated string to be transferred, Output: none */
 void LCD_OutString(char *pt){
   while(*pt){
     LCD_OutChar(*pt);
@@ -75,80 +70,71 @@ void LCD_OutString(char *pt){
   }
 }
 
-//-----------------------LCD_OutUDec-----------------------
-// Output a 32-bit number in unsigned decimal format
-// Input: 32-bit number to be transferred
-// Output: none
-// Variable format 1-10 digits with no space before or after
-void LCD_OutUDec(uint32_t n){
-// This function uses recursion to convert decimal number of unspecified length as an ASCII string
+/* ============================ LCD_OutUDec ============================ */
+/* Output a 32-bit number in unsigned decimal format, Input: 32-bit number to be transferred, Output: none
+   Variable format 1-10 digits with no space before or after */
+void LCD_OutUDec(uint32_t n){ 				
+/*This function uses recursion to convert decimal number of unspecified length as an ASCII string */
   if(n >= 10){
     LCD_OutUDec(n/10);
     n = n%10;
   }
-  LCD_OutChar(n+'0'); // n is between 0 and 9 
+  LCD_OutChar(n+'0');							/* n is between 0 and 9 */
 }
 
 volatile uint32_t ui32SysClkFreq;
-void PortFunctionInit(void)
-{
-//
-	volatile uint32_t ui32Loop;   // Enable the clock of the GPIO port N that is used for the on-board LED D1 
-	ui32SysClkFreq = SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ | SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480), 120000000); // 120MHz
+
+void PortFunctionInit(void){
+	ui32SysClkFreq = SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ | SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480), 120000000);
+									/* 120MHz */
+  	SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R9|SYSCTL_RCGCGPIO_R4|SYSCTL_RCGCGPIO_R12;
+  									/* Do a dummy read to insert a few cycles after 
+  									enabling the peripheral */
+  	ui32Loop = SYSCTL_RCGCGPIO_R;
+	GPIO_PORTN_DIR_R |= 0x01;					/* PN0 direction as output */
+	GPIO_PORTN_DEN_R |= 0x01;					/* PN0 data enable */
 	
-  SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R9|SYSCTL_RCGCGPIO_R4|SYSCTL_RCGCGPIO_R12;
-  // Do a dummy read to insert a few cycles after enabling the peripheral
-  ui32Loop = SYSCTL_RCGCGPIO_R;
+	GPIO_PORTK_AMSEL_R &= ~0xFF;					/* disable analog function on PK7-0 */
+	GPIO_PORTE_AHB_AMSEL_R &= ~0x30;				/* disable analog function on PE5-4 */
 	
-	GPIO_PORTN_DIR_R |= 0x01;//PN0
-	GPIO_PORTN_DEN_R |= 0x01;//PN0
+	GPIO_PORTK_PCTL_R = 0x00000000;					/* configure PK7-0 as GPIO */
+  	GPIO_PORTE_AHB_PCTL_R &= ~0xFF000000;				/* configure PA7-6 as GPIO */
+	GPIO_PORTK_DIR_R = 0xFF;					/* set direction register */
+  	GPIO_PORTE_AHB_DIR_R |= 0x30;
+	GPIO_PORTK_AFSEL_R = 0x00;					/* regular port function */
+  	GPIO_PORTE_AHB_AFSEL_R &= ~0x30;
+	GPIO_PORTK_DEN_R = 0xFF;					/* enable digital port */
+  	GPIO_PORTE_AHB_DEN_R |= 0x30;
+	GPIO_PORTK_DR8R_R = 0xFF;					/* enable 8 mA drive */
+  	GPIO_PORTE_AHB_DR8R_R |= 0x30;
 	
-	GPIO_PORTK_AMSEL_R &= ~0xFF;   // 3) disable analog function on PK7-0
-	GPIO_PORTE_AHB_AMSEL_R &= ~0x30;   //    disable analog function on PE5-4  
-	
-	GPIO_PORTK_PCTL_R = 0x00000000;   // 4) configure PK7-0 as GPIO   
-  GPIO_PORTE_AHB_PCTL_R &= ~0xFF000000; //    configure PA7-6 as GPIO
-	
-	GPIO_PORTK_DIR_R = 0xFF;       // 5) set direction register
-  GPIO_PORTE_AHB_DIR_R |= 0x30;
-	
-	GPIO_PORTK_AFSEL_R = 0x00;     // 6) regular port function
-  GPIO_PORTE_AHB_AFSEL_R &= ~0x30;
-	
-	GPIO_PORTK_DEN_R = 0xFF;       // 7) enable digital port
-  GPIO_PORTE_AHB_DEN_R |= 0x30;
-	
-	GPIO_PORTK_DR8R_R = 0xFF;      // enable 8 mA drive
-  GPIO_PORTE_AHB_DR8R_R |= 0x30;
-	
-  LCDCMD = 0;           // E=0, R/W=0, RS=0
-  SysCtlDelay(600000);		//SysTick_Wait(T15ms);  // Wait >15 ms after power is applied
-  OutCmd(0x30);         // command 0x30 = Wake up
-  SysCtlDelay(200000);		//SysTick_Wait(T5ms);   // must wait 5ms, busy flag not available
-  OutCmd(0x30);         // command 0x30 = Wake up #2
-  SysCtlDelay(6400);		//SysTick_Wait(T160us); // must wait 160us, busy flag not available
-  OutCmd(0x30);         // command 0x30 = Wake up #3
-  SysCtlDelay(6400);		//SysTick_Wait(T160us); // must wait 160us, busy flag not available
-  OutCmd(0x38);         // Function set: 8-bit/2-line
-  OutCmd(0x10);         // Set cursor
-  OutCmd(0x0C);         // Display ON; Cursor ON
-  OutCmd(0x06);         // Entry mode set
+	LCDCMD = 0;							/* E=0, R/W=0, RS=0 */
+	SysCtlDelay(600000);						/* Wait >15 ms after power is applied */
+	OutCmd(0x30);							/* command 0x30 = Wake up */
+	SysCtlDelay(200000);						/*must wait 5ms, busy flag not available */
+	OutCmd(0x30);							/* command 0x30 = Wake up #2 */
+	SysCtlDelay(6400);						/* must wait 160us, busy flag not available */
+	OutCmd(0x30);							/* command 0x30 = Wake up #3 */
+	SysCtlDelay(6400);						/* must wait 160us, busy flag not available */
+	OutCmd(0x38);							/* Function set: 8-bit/2-line */
+	OutCmd(0x10);							/* Set cursor */
+	OutCmd(0x0C);							/* Display ON; Cursor ON */
+	OutCmd(0x06);							/* Entry mode set */
 }
 
-int main(void)
-{
-	PortFunctionInit();
-	GPIO_PORTN_DATA_R |= 0x01;
-	SysCtlDelay(200000);
-	LCD_Clear();
-	LCD_OutString("sfsu.edu");
-	SysCtlDelay(80000000);
-	while(1)
+int main(void){
+	PortFunctionInit();						/* Initialize ports */
+	GPIO_PORTN_DATA_R |= 0x01;					/*  LED ON */
+	SysCtlDelay(200000);						/* Give some delay */
+	LCD_Clear();							/* Clear the LCD screen */
+	LCD_OutString("sfsu.edu");					/* Write a string on the screen */
+	SysCtlDelay(80000000);						/* keep the string for some time on the screen */
+	while(1)							/* Infinite while loop */
 	{
-		GPIO_PORTN_DATA_R ^= 0x01;
-		SysCtlDelay(40000000);
-		LCD_Clear();
-		LCD_OutString("SFSU.edu");
-		SysCtlDelay(80000000);
+		GPIO_PORTN_DATA_R ^= 0x01;				/* keep blinking the LED at PN1 */
+		SysCtlDelay(40000000);					/* give some delay for blink */
+		LCD_Clear();						/* clear the LCD */
+		LCD_OutString("SFSU.edu");				/* show a string on LCD */
+		SysCtlDelay(80000000);					/* delay to keep the string on LCD */
 	}
 }
